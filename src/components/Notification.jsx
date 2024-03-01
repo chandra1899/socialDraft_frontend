01161af2fab39d3fcb3740d3b51939d3733106b2
chandra <101719497+chandra1899@users.noreply.github.com>
2024-03-01 13:21:08 +0530

    created function to fetch notifications and navigate to post

--- src/components/Notification.jsx
+++ src/components/Notification.jsx
@@ -1,39 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import config from '../source'
+import { useNavigate } from 'react-router'
 
-const SubNotification = () => {
+const SubNotification = ({notification}) => {
+  const navigate=useNavigate()
     let dark=true
-    let notification={
-    type:'Messaged'
-    }
   return (
     <>
     
-    {notification.type==='Liked' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
-        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#c4746665@gmail.com</p>
+    {notification.typeOf==='LikedPost' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
         <p className='text-[0.9rem] ml-2 mt-1 text-red-600'>Liked Your Post</p>
         <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>No one able to ...</p>
       </div> }
 
-    {notification.type==='Commented' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
-        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#c4746665@gmail.com</p>
+    {notification.typeOf==='LikedComment' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
+        <p className='text-[0.9rem] ml-2 mt-1 text-red-600'>Liked Your Comment</p>
+        <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>{notification.LikedPost.content?.length>=13?`${notification.LikedPost.content.substring(0, 13)} ...`:`${notification.LikedPost.content}`}</p>
+      </div> }
+
+    {notification.typeOf==='Commented' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
         <p className='text-[0.9rem] ml-2 mt-1 text-[#f4c838]'>Commented Your Post</p>
         <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>No one able to ...</p>
       </div> }
 
-    {notification.type==='Retweeted' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
-        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#c4746665@gmail.com</p>
+    {notification.typeOf==='Retweeted' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
         <p className='text-[0.9rem] ml-2 mt-1 text-[#3a3afb]'>Retweeted</p>
-        <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>No one able to ...</p>
+        <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>{notification.Retweeted.retweetedRef.content?.length>=13?`${notification.Retweeted.retweetedRef.content.substring(0, 13)} ...`:`${notification.Retweeted.retweetedRef.content}`}</p>
       </div> }
 
-      {notification.type==='Posted' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
-        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#c4746665@gmail.com</p>
+      {notification.typeOf==='Posted' && <div className='bg-slate-900 p-2 rounded-lg my-1 cursor-pointer hover:bg-slate-950' onClick={()=>{navigate(`/post/${notification.Posted._id}`)}} >
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
         <p className='text-[0.9rem] ml-2 mt-1 text-[#3a3afb]'>Posted</p>
-        <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>No one able to ...</p>
+        <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>{notification.Posted.content?.length>=13?`${notification.Posted.content.substring(0, 13)} ...`:`${notification.Posted.content}`}</p>
       </div> }
 
-    {notification.type==='Messaged' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
-        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#c4746665@gmail.com</p>
+    {notification.typeOf==='Messaged' && <div className='bg-slate-900 p-2 rounded-lg my-1'>
+        <p className={`text-[0.95rem] ${dark?"text-[#42a5c6]":"text-[#674bf3]"}`}>#{notification.fromEmail}</p>
         <p className='text-[0.9rem] ml-2 mt-1 text-[#3ff339]'>Messaged</p>
         <p className='text-[0.85rem] ml-3 mt-1 border-t-2 border-l-2 border-slate-700 m-1 p-1'>No one able to ...</p>
       </div> }
@@ -42,20 +48,32 @@ const SubNotification = () => {
 }
 
 const Notification = () => {
-    
+    const [notifications, setNotifications] = useState([]);
+  const getNotifications = async ()=>{
+    let res=await fetch(`${config.baseUrl}/api/notification/getnotifications`,{
+      method:"GET",
+      headers:{
+        "Content-Type":"application/json"
+      },
+    credentials:'include', 
+
+    })
+    let data=await res.json();
+    if(res.status===200){
+      console.log(data.notifications);
+      setNotifications(data.notifications)
+    }
+  }
+
+  useEffect(()=>{
+    getNotifications()
+  },[])
+
   return (
     <div className='absolute right-[18rem] top-16 rounded-xl w-[23vw] h-[100vh] bg-slate-800 z-[1] p-2 flex flex-col overflow-scroll no-scrollbar'>
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
-      <SubNotification />
+      {notifications.map((notification,i)=>(
+        <SubNotification key={i} notification={notification}/>
+        ))}
     </div>
   )
 }
